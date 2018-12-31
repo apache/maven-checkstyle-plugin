d3945014812d253faa6303f42fc1b2a6757846d9
Robert Turner <rturner@e-djuster.ca>
2018-12-31 17:05:28 -0500

    [MCHECKSTYLE-365] Correct counts in Rules section
    When the site report is generated, in the Rules section, the plug-in
    groups together the violation counts for rules that have the same name
    and message, but that have different severity levels – specifically
    where one of the rules doesn't specify a severity level and uses the
    default (error). This results in both rules being listed with different
    properties and severity levels, but with the same violation count (which
    is incorrect).
    
    This change allows the rule matching to fall through to the most
    specific match rather than short-circuiting the match on success. This
    corrects the case where two rules of the same name have the same message
    but different severities and allows the match to proceed to check
    further conditions, such as the severity.
    
    In addition, this change also ensures that the severity is retrieved in
    the same way (by providing a default of "error" when missing) as the
    logic that displays the rule rows in the Rules section.
    
    Both of these changes are required to properly correct the issue.

---
+++ src/it/MCHECKSTYLE-365/verify.groovy
@@ -0,0 +1,45 @@
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+def htmlReportFile = new File(basedir, 'target/site/checkstyle.html');
+assert htmlReportFile.exists();
+
+/*
+ * These two test cases will both return 3 if the bug isn't fixed
+ * (1 for the violation, and 1 each for the 2 rules).
+ *
+ * If fixed, the count should be 2 (1 for the violation, and 1 for
+ * the Rule summary)
+ *
+ * Note that the angle-brackets at either end are to count only
+ * the user visible text as opposed to the occurances in the URLs.
+ *
+ * A more robust solution would be to parse the HTML, but that's
+ * a lot more effort than required to actually verify the behaviour.
+ */
+
+// Case with no custom messages
+def numberOfOccurancesOfFileTabCharacter = htmlReportFile.text.count(">FileTabCharacter<")
+assert 2 == numberOfOccurancesOfFileTabCharacter;
+
+// Case with custom messages
+def numberOfOccurancesOfRegexpSingleline = htmlReportFile.text.count(">RegexpSingleline<");
+assert 2 == numberOfOccurancesOfRegexpSingleline;
+
+return true;
